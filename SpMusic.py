2cf4cf92bcae45fd15e309596bba596be37b1d99
samoylikv <vasya.samoylik@gmail.com>
2024-04-03 00:06:17 +0400

    Ya Music and Spotify get liked done

--- SpMusic.py
+++ SpMusic.py
@@ -2,9 +2,9 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from constants import SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIRECT_URI
 import json
-import os
 import logging
 import colorlog
+from utilities import track_list, refresh_tracks
 
 handler = colorlog.StreamHandler()
 handler.setFormatter(colorlog.ColoredFormatter(
@@ -37,43 +37,15 @@ sp = spotipy.Spotify(auth_manager=auth_manager)
 filename = 'spotify_tracks.json'
 
 logger.info("Загрузка треков из Spotify...")
-if os.path.exists(filename):
-    with open(filename, 'r', encoding='utf-8') as file:
-        spotify_tracks = json.load(file)
-else:
-    spotify_tracks = []
-
-
-def track_exists(artist_name, track_name):
-    for track in spotify_tracks:
-        if track['artist'] == artist_name and track['track_name'] == track_name:
-            return True
-    return False
-
-
-def refresh_tracks(results):
-    global counter
-    for item in results['items']:
-        track = item['track']
-        artist_name = track['artists'][0]['name']
-        track_name = track['name']
-        if not track_exists(artist_name, track_name):
-            counter += 1
-            logger.info(f"Добавление трека {track_name} исполнителя {artist_name} в список.")
-            spotify_tracks.append({'artist': artist_name, 'track_name': track_name})
+spotify_tracks = track_list(filename)
 
 
 results = sp.current_user_saved_tracks()
-refresh_tracks(results)
-
+res_tracks = []
 while results['next']:
     results = sp.next(results)
-    refresh_tracks(results)
-if counter == 0:
-    logger.info("Треки не обновлены.")
-else:
-    with open(filename, 'w', encoding='utf-8') as file:
-        logger.info(f"Сохранение треков в {filename}...")
-        json.dump(spotify_tracks, file, ensure_ascii=False, indent=4)
+    res_tracks.append(refresh_tracks(results, spotify_tracks, filename, 'spotify'))
 
-    logger.info(f"Добавлено {counter} треков.")
+
+with open(filename, 'w', encoding='utf-8') as file:
+    json.dump(spotify_tracks, file, ensure_ascii=False, indent=4)
--- YaMusic.py
+++ YaMusic.py
@@ -2,6 +2,7 @@ from yandex_music import Client
 from constants import YA_TOKEN
 import logging
 import colorlog
+from utilities import track_list, refresh_tracks
 
 handler = colorlog.StreamHandler()
 handler.setFormatter(colorlog.ColoredFormatter(
@@ -24,18 +25,9 @@ logger.setLevel(logging.INFO)
 logger.addHandler(handler)
 
 
-client = Client()
 client = Client(YA_TOKEN).init()
-client.users_likes_tracks()[0].fetch_track()
 tracks = client.users_likes_tracks().fetchTracks()
 
-for track in tracks:
-    artists = track.artists
-    if len(artists) > 1:
-        artists = ', '.join([artist.name for artist in artists])
-    else:
-        artists = artists[0].name
-    title = track.title
-    print(title)
-    # print(track.artistsName, '-', track.title)
-
+filename = 'yandex_tracks.json'
+yandex_tracks = track_list(filename)
+refresh_tracks(tracks, yandex_tracks, filename, 'yandex')
